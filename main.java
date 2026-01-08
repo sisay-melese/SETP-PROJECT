532ccae5918bb093a9d9122d709bc54d2dcc7494
mdmirikat-rgb <mdmirikat@gmail.com>
2026-01-09 00:24:09 +0300

    update main function
    Refactored variable names to follow Java naming conventions and added prompts for book price and page number.

--- main.java
+++ main.java
@@ -1,22 +1,38 @@
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
-         Scanner scanner=new Scanner(System.in);
-        System.out.println("Please enter book title");
-        String Title=scanner.nextLine();
-        System.out.println("Please enter book author");
-        String Author=scanner.nextLine();
-        System.out.println("Please enter member name");
-        String MemberName=scanner.nextLine();
-        System.out.println("Please enter member Id");
-        String MemberId=scanner.nextLine();
 
-        book book1 = new book(Title,Author);
-        member member1 = new member(MemberId, MemberName);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter book title");
+        String title = scanner.nextLine();
+
+        System.out.println("Please enter book author");
+        String author = scanner.nextLine();
+
+        System.out.println("Please enter member name");
+        String memberName = scanner.nextLine();
+
+        System.out.println("Please enter member Id");
+        String memberId = scanner.nextLine();
+
+        System.out.println("Please enter book price");
+        double bookPrice = scanner.nextDouble();
+
+        System.out.println("Please enter book page number");
+        int bookPageNumber = scanner.nextInt();
+
+        Book book1 = new Book(title, author);
+        Member member1 = new Member(memberId, memberName);
+
         book1.displayStatus();
         member1.displayMember();
 
+        // Display extra details
+        System.out.println("Book Price: " + bookPrice);
+        System.out.println("Book Page Number: " + bookPageNumber);
 
+        scanner.close();
     }
 }
